a1eebd36a7440e3c45417d6b1a8a372b154de4d6
csefenil <82444829+csefenil@users.noreply.github.com>
2021-04-14 22:38:08 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+
+    imp = dict(request.form)
+
+    int_features = [float(x) for x in imp.values() if not x.isalpha()]
+    print(int_features)
+    final_features = [np.array(int_features)]
+    print(final_features)
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0],2)
+
+    return render_template('index.html', prediction_text='House Rent Should be {}Rs.'.format(output))
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
